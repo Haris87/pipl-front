2dc1f3a78722afbc7dc97d0d6a8720258941fd4c
Haris87 <mpouxlhs.xar@gmail.com>
2019-11-09 14:25:27 +0200

    layout components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,23 +1,35 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { PersonComponent } from './person/person.component';
-import { PersonListComponent } from './person-list/person-list.component';
-import { AuthGuard } from './guards/auth-guard.service';
-import { LoginComponent } from './login/login.component';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { PersonComponent } from "./person/person.component";
+import { PersonListComponent } from "./person-list/person-list.component";
+import { AuthGuard } from "./guards/auth-guard.service";
+import { LoginComponent } from "./login/login.component";
+import { FooterOnlyLayoutComponent } from "./footer-only-layout/footer-only-layout.component";
+import { MainLayoutComponent } from "./main-layout/main-layout.component";
 
 const routes: Routes = [
   {
-    path: 'people',
-    component: PersonListComponent,
-    canActivate: [AuthGuard]
+    path: "",
+    component: MainLayoutComponent,
+    children: [
+      {
+        path: "people",
+        component: PersonListComponent,
+        canActivate: [AuthGuard]
+      },
+      {
+        path: "people/:id",
+        component: PersonComponent,
+        canActivate: [AuthGuard]
+      }
+    ]
   },
   {
-    path: 'people/:id',
-    component: PersonComponent,
-    canActivate: [AuthGuard]
+    path: "login",
+    component: FooterOnlyLayoutComponent,
+    children: [{ path: "", component: LoginComponent }]
   },
-  { path: 'login', component: LoginComponent },
-  { path: '', redirectTo: '/', pathMatch: 'full' }
+  { path: "", redirectTo: "/", pathMatch: "full" }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,33 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { AngularFireModule } from '@angular/fire';
-import { AngularFireAuthModule } from '@angular/fire/auth';
-import { AuthenticationService } from './shared/authentication.service';
-import { environment } from '../environments/environment';
-import { AppComponent } from './app.component';
-import { FormsModule } from '@angular/forms';
-import { AuthenticationComponent } from './authentication/authentication.component';
-import { AppRoutingModule } from './app-routing.module';
-import { PersonComponent } from './person/person.component';
-import { PersonListComponent } from './person-list/person-list.component';
-import { PersonService } from './shared/person.service';
-import { LoginComponent } from './login/login.component';
-import { AuthGuard } from './guards/auth-guard.service';
-import { MeComponent } from './me/me.component';
+import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { NgModule } from "@angular/core";
+import { AngularFireModule } from "@angular/fire";
+import { AngularFireAuthModule } from "@angular/fire/auth";
+import { AuthenticationService } from "./shared/authentication.service";
+import { environment } from "../environments/environment";
+import { AppComponent } from "./app.component";
+import { FormsModule } from "@angular/forms";
+import { AuthenticationComponent } from "./authentication/authentication.component";
+import { AppRoutingModule } from "./app-routing.module";
+import { PersonComponent } from "./person/person.component";
+import { PersonListComponent } from "./person-list/person-list.component";
+import { PersonService } from "./shared/person.service";
+import { LoginComponent } from "./login/login.component";
+import { AuthGuard } from "./guards/auth-guard.service";
+import { MeComponent } from "./me/me.component";
+import { LayoutModule } from "@angular/cdk/layout";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatButtonModule } from "@angular/material/button";
+import { MatSidenavModule } from "@angular/material/sidenav";
+import { MatIconModule } from "@angular/material/icon";
+import { MatListModule } from "@angular/material/list";
+import { MatGridListModule } from "@angular/material/grid-list";
+import { MatCardModule } from "@angular/material/card";
+import { MatMenuModule } from "@angular/material/menu";
+import { MainLayoutComponent } from './main-layout/main-layout.component';
+import { FooterOnlyLayoutComponent } from './footer-only-layout/footer-only-layout.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
@@ -22,14 +36,28 @@ import { MeComponent } from './me/me.component';
     PersonComponent,
     PersonListComponent,
     LoginComponent,
-    MeComponent
+    MeComponent,
+    MainLayoutComponent,
+    FooterOnlyLayoutComponent,
+    FooterComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireAuthModule,
-    FormsModule
+    BrowserAnimationsModule,
+    FormsModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    MatGridListModule,
+    MatCardModule,
+    MatMenuModule
   ],
   providers: [AuthenticationService, PersonService, AuthGuard],
   bootstrap: [AppComponent]
